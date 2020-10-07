93374a16b3e07754b585d97bc331a177ec0c7fc2
lamilekan263 <lamilekan263@gmail.com>
2020-10-07 08:19:45 +0100

    added a google search api key

---
+++ src/keys.js
@@ -0,0 +1,2 @@
+export const API_KEY = "AIzaSyApFnvZYB5uZLBxJ6XsjI4QQ-KO04EGKYI";
+export default API_KEY
--- src/pages/searchPage/SearchPage.js
+++ src/pages/searchPage/SearchPage.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import "./SearchPage.css";
+import { useStateValue } from '../../reducer/StateProvider'
 
 function Search() {
+
+    const [{ term }, dispatch] = useStateValue()
+    
     return (
         <div className='searchPage'>
-            
-        </div>
+            <div className='.searchPage__header'>
+                <h1>{term}</h1>
+            </div>
+            <div className='searchPage__result'>
+        
+            </div>
+        </div> 
     )
 }
 
