faf3c640cc54acb97eb171861def9105200a6bef
lamilekan263 <lamilekan263@gmail.com>
2020-10-07 10:34:54 +0100

    styling continues

--- src/pages/searchPage/SearchPage.js
+++ src/pages/searchPage/SearchPage.js
@@ -24,59 +24,75 @@ function Search() {
     console.log(data)
     
     return (
-        <div className="searchPage">
-            <div className="searchPage__header">
-                <Link to="/">
-                    <img
-                        className="searchPage__logo"
-                        src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
-                        alt="google logo"
-                    />
-                </Link>
-                <div className="searchPage__headerBody">
-                    <SearchInput hideButtons />
-                    <div className="searchPage__options">
-                        <div className="searchPage__optionsLeft">
-                            <div className="searchPage__option">
-                                <SearchIcon />
-                                <Link to="/all">All</Link>
-                            </div>
-                            <div className="searchPage__option">
-                                <DescriptionIcon />
-                                <Link to="/news">News</Link>
-                            </div>
-                            <div className="searchPage__option">
-                                <ImageIcon />
-                                <Link to="/images">Images</Link>
-                            </div>
-                            <div className="searchPage__option">
-                                <LocalOfferIcon />
-                                <Link to="/shopping">Shopping</Link>
-                            </div>
-                            <div className="searchPage__option">
-                                <RoomIcon />
-                                <Link to="/maps">maps</Link>
-                            </div>
-                            <div className="searchPage__option">
-                                <MoreVertIcon />
-                                <Link to="/more">more</Link>
-                            </div>
-                        </div>
-                    <div className="searchPage__optionsRight">
-                        <div className="searchPage__option">
-                            <Link to="/settings">Settings</Link>
-                        </div>
-                        <div className="searchPage__option">
-                            <Link to="/tools">Tools</Link>
-                        </div>
-                    </div>
-                    </div>
+      <div className="searchPage">
+        <div className="searchPage__header">
+          <Link to="/">
+            <img
+              className="searchPage__logo"
+              src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+              alt="google logo"
+            />
+          </Link>
+          <div className="searchPage__headerBody">
+            <SearchInput hideButtons />
+            <div className="searchPage__options">
+              <div className="searchPage__optionsLeft">
+                <div className="searchPage__option">
+                  <SearchIcon />
+                  <Link to="/all">All</Link>
                 </div>
+                <div className="searchPage__option">
+                  <DescriptionIcon />
+                  <Link to="/news">News</Link>
+                </div>
+                <div className="searchPage__option">
+                  <ImageIcon />
+                  <Link to="/images">Images</Link>
+                </div>
+                <div className="searchPage__option">
+                  <LocalOfferIcon />
+                  <Link to="/shopping">Shopping</Link>
+                </div>
+                <div className="searchPage__option">
+                  <RoomIcon />
+                  <Link to="/maps">maps</Link>
+                </div>
+                <div className="searchPage__option">
+                  <MoreVertIcon />
+                  <Link to="/more">more</Link>
+                </div>
+              </div>
+              <div className="searchPage__optionsRight">
+                <div className="searchPage__option">
+                  <Link to="/settings">Settings</Link>
+                </div>
+                <div className="searchPage__option">
+                  <Link to="/tools">Tools</Link>
+                </div>
+              </div>
             </div>
-            <div className="searchPage__result">
-                
-         </div>
-       </div>
+          </div>
+        </div>
+        {term && (
+          <div className="searchPage__results">
+            <p className="searchPage__resultCount">
+              About {data?.searchInformation.formattedTotalResults} results (
+              {data?.searchInformation.formattedSearchTime} seconds) for {term}
+            </p>
+            {data?.items.map((item) => (
+              <div className="searchPage__result">
+                <a href={item.link}>{item.displayLink}</a>
+                    <a href={item.link} className='searchPage__resultTitle'>
+                        <h2>{item.title}</h2>
+                    </a>
+                    <p classname='searchPage__resultSnippet'>
+                        {item.snippet}
+                    </p>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     );
 }
 
